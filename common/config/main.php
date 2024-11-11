fbac2664128b8e33c22b7d1ac0661b13ba1118f5
atynov <atynov@mail.ru>
2024-11-11 14:41:39 +0500

    1

--- common/config/main.php
+++ common/config/main.php
@@ -9,7 +9,7 @@ use modules\organization\Module as OrganizationModule;
 
 
 return [
-    'name' => 'Advanced',
+    'name' => 'Advanced2',
     'timeZone' => 'Asia/Almaty',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
